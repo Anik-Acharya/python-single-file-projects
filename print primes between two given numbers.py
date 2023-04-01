171de19654bf1a94ab7dd6a6db2d47163f76cb77
Anik Acharya <anikacharya5@gmail.com>
2023-04-01 14:03:50 +0530

    Some improvements

--- print primes between two given numbers.py
+++ print primes between two given numbers.py
@@ -6,3 +6,10 @@ def isPrime(num):
             if num%i==0:
                 return False
         return True
+
+a=int(input("enter the lower bound: "))
+b=int(input("enter the upper bound: "))
+
+for i in range(a,b+1):
+    if isPrime(i):
+        print(i)
